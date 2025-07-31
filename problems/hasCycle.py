3c141e63325f58e912bd92a2b9c24341b7f48916
Fahd <fahd@fahd.fahd>
2025-07-31 19:02:43 -0400

    [add] hasCycle, reverseLinkedList

---
+++ problems/hasCycle.py
@@ -0,0 +1,31 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+# Time -> O(n)
+# Space -> O(1)
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        if not head or not head.next:
+            return False
+        
+        slow = head.next
+        fast = head.next.next
+
+        while slow and fast and fast.next:
+            if slow == fast:
+                return True
+            
+            slow = slow.next
+            fast = fast.next.next
+        
+        return False
