13c7039d4ea76970c207c094267aa3aca3bbf7d5
franksheikh <franksheikh@icloud.com>
2025-08-14 20:02:43 -0400

    [add] subtreeAnotherTree.py

--- problems/sameTree.py
+++ problems/sameTree.py
@@ -8,14 +8,19 @@
 # Recursive
 class Solution:
     def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        if not p and not q:
+            return True
         if not p and q or not q and p:
             return False
-		elif p.val != q.val:
-			return False	
+        if p.val != q.val:
+            return False
+        
+        return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
+            
         
 
 # iterative
-class Solution:
+class Solution2:
     def isSameTree2(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
         if p and not q or q and not p:
             return False
