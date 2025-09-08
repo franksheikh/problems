ce68c79fd1467fa2ef3ac6925e21813169a37734
franksheikh <franksheikh@icloud.com>
2025-09-08 14:10:02 -0400

    [add] problem updates

--- problems/balancedBinaryTree.py
+++ problems/balancedBinaryTree.py
@@ -1,3 +1,36 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def checkHeight(self, node):
+        if not node:
+            return 0
+        
+        left = self.checkHeight(node.left)
+        if left == -1:
+            return -1
+        right = self.checkHeight(node.right)
+        if right == -1:
+            return -1
+        
+
+        
+        if abs(left - right) > 1:
+            return -1
+        
+        return max(left, right) + 1
+
+    def isBalanced(self, root: Optional[TreeNode]) -> bool:
+        if not root:
+            return True
+        return self.checkHeight(root) != -1
+        
+        
+
 '''
 [
     (2,1),
--- problems/validParentheses.py
+++ problems/validParentheses.py
@@ -1,4 +1,23 @@
+
 # Solution 1
+# Push only opening brackets, or unclosed brackets
+class Solution:
+    def isValid(self, s: str) -> bool:
+        chars = {
+            ']':'[',
+            ')':'(',
+            '}':'{'
+        }
+        ans = []
+        for char in s:
+            if char in chars:
+                if not ans or ans.pop() != chars[char]:
+                    return False
+            else:
+                ans.append(char)
+        return not ans
+
+# Solution 2
 # Time -> O(n)
 # Space -> O(n)
 
@@ -21,7 +40,7 @@ class Solution:
             
         return len(l) == 0
 
-# Solution 2
+# Solution 3
 # Time -> O(n)
 # Space -> O(n)
 
