a837590e6515e98dfd204f9b6ed4712df45af5c3
franksheikh <asadmansa@icloud.com>
2025-09-08 14:10:02 -0400

    [add] problem updates

--- problems/2352. Equal Row and Column Pairs.py
+++ problems/2352. Equal Row and Column Pairs.py
@@ -1,3 +1,14 @@
+'''
+If the grid has a size of n times n, this algorithm has a time complexity of O of n^2 - there are n^2 elements and each element is iterated over twice initially (once for the row it occupies and once for the column it occupies). 
+
+Populating and then iterating over the hash maps will be dominated by this. 
+
+The space complexity is O of n^2 - if all rows and columns are unique, then each of the two hash maps will both grow to a size of n, with each key having a length of n.
+
+TKWY:
+- Identify solution before coding
+- Use naive/simplest approach first.
+'''
 class Solution:
     def equalPairs(self, grid: List[List[int]]) -> int:
         max_n = 0
