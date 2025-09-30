c8aeca7a39331124f86b10e313efa6aabd14807e
franksheikh <franksheikh@outlook.com>
2025-09-30 17:50:31 -0400

    2260

--- problems/49. Group Anagrams.py
+++ problems/49. Group Anagrams.py
@@ -1,3 +1,12 @@
+'''
+Given n n as the length of strs and m m as the average length of the strings, we iterate over each string and sort it, which costs O(n⋅m⋅logm). Then, we need to iterate over the keys. 
+
+In the worst case scenario, when there are no matching anagrams, there will be n n groups, which means this will cost O ( n ) O(n), giving an overall time complexity of O(n⋅m⋅logm) (the final + n +n is dominated). 
+
+The space complexity is O ( n ⋅ m ) O(n⋅m) as each string will be placed in an array within the hash map.
+
+'''
+
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         strings = defaultdict(list)
