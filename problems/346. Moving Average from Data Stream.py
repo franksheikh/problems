dea09294f8e4f44acf2e911992c96499497f76cd
franksheikh <franksheikh@icloud.com>
2025-10-06 16:49:33 -0400

    346

---
+++ problems/346. Moving Average from Data Stream.py
@@ -0,0 +1,29 @@
+class MovingAverage:
+
+    def __init__(self, size: int):
+        self.max_size = size
+        self.values = collections.deque()
+        self.ps = 0
+
+        
+
+    def next(self, val: int) -> float:
+        self.ps += val
+        self.values.append(val)
+
+        if self.values and len(self.values) > self.max_size:
+            last_val = self.values.popleft()
+            self.ps -= last_val
+        
+        return self.ps / len(self.values)
+
+
+
+
+
+        
+
+
+# Your MovingAverage object will be instantiated and called as such:
+# obj = MovingAverage(size)
+# param_1 = obj.next(val)
