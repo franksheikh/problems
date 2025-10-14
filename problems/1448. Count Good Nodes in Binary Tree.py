05df3951ca52f8a0924764d7499636982260c2cc
franksheikh <franksheikh@icloud.com>
2025-10-14 18:01:53 -0400

    100

--- problems/1448. Count Good Nodes in Binary Tree.py
+++ problems/1448. Count Good Nodes in Binary Tree.py
@@ -20,4 +20,33 @@ class Solution:
         
         return traverse(root, 0)
         
+
+# 2 Official
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def goodNodes(self, root: TreeNode) -> int:
+        if not root:
+            return 0
+
+        def traverse(node, local_max):
+            if not node:
+                return 0
+            
+            new_max = max(local_max, node.val)
+            left = traverse(node.left, new_max)
+            right = traverse(node.right, new_max)
+
+            ans = left + right
+            if node.val >= local_max:
+                ans += 1
+            return ans
+
         
+        return traverse(root, -inf)
+
+
