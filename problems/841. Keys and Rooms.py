0f331e40854aefdb31ff6c627fdd3dece14bd063
franksheikh <franksheikh@icloud.com>
2025-10-28 14:00:20 -0400

    841

---
+++ problems/841. Keys and Rooms.py
@@ -0,0 +1,33 @@
+'''
+Time - O(n) 
+- At most, we visit every room once
+
+Space - O(n)
+- We build a graph with N nodes and, at most, N keys. So O(n + n) => O(n)
+
+'''
+class Solution:
+    def canVisitAllRooms(self, rooms: List[List[int]]) -> bool:
+        n = len(rooms)
+        graph = {room:[] for room in range(n)}
+        seen = set()
+
+        def dfs(room):
+            for key in graph[room]:
+                if key not in seen:
+                    seen.add(key)
+                    dfs(key)
+
+        
+        for i in range(n):
+            room = rooms[i]
+            for j in range(len(room)):
+                graph[i].append(room[j])
+        
+        seen.add(0)
+        dfs(0)
+
+        return len(seen) == n
+
+
+        
