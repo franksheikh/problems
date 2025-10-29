8608c568171f98dcc76d6a024aeb053dfed83bce
franksheikh <franksheikh@icloud.com>
2025-10-29 12:28:33 -0400

    1971

---
+++ problems/1971. Find if Path Exists in Graph.py
@@ -0,0 +1,50 @@
+'''
+
+edges:
+[
+    [0,1],
+    [1,2],
+    [2,0]
+]
+
+
+seen set
+
+1. create graph of {node: vertices}
+
+{
+    0: [1,2]
+    1: []
+    2: []
+}
+
+2. dfs(source)
+    - if our neighbor not in seen (prevent repeats):
+        - if current node == destination, reassign found to true
+
+call dfs(source)
+
+3. default case return found (initialized to false)
+
+'''
+
+class Solution:
+    def validPath(self, n: int, edges: List[List[int]], source: int, destination: int) -> bool:
+        seen = set()
+        graph = {i:[] for i in range(n)}
+        for ui, vi in edges:
+            graph[ui].append(vi)
+            graph[vi].append(ui)
+        
+        def dfs(node):
+            if node == destination:
+                return True
+            for neighbor in graph[node]:
+                if neighbor not in seen:
+                    seen.add(neighbor)
+                    if dfs(neighbor):
+                        return True
+            return False
+        seen.add(source)
+        return dfs(source)
+        
